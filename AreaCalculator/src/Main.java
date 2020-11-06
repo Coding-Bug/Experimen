459ece2c25747cb3b943a1f5aca04c417250fd88
Coding-Bug <accjf@outlook.com>
2020-11-06 16:57:31 +0800

    javaSE课程实验代码

--- AreaCalculator/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Circle circle = new Circle(5);
-        Trapezoid trapezoid = new Trapezoid(12,11,3);
-        circle.print();
-        trapezoid.print();
-    }
-
-}
